f5f28102818990ddd12ba453c2d01644b03ad6c7
Jing CHEN <307838490@qq.com>
2019-02-10 22:33:58 +0000

    init

--- rango/views.py
+++ rango/views.py
@@ -9,19 +9,31 @@ from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 
 # Create your views here.
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list,'pages':page_list}
-    return render(request, 'rango/index.html', context_dict)
+    context_dict = {'categories': category_list, 'pages':page_list}
+    
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context_dict)
+    return response
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    context_dict = {}    
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
     print(request.method)
     print(request.user)
-    return render(request, 'rango/about.html', {})
+    return render(request, 'rango/about.html', context_dict)
  
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -35,7 +47,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     
     return render(request, 'rango/category.html', context_dict)
-
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -47,7 +59,7 @@ def add_category(request):
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
-
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -126,8 +138,26 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))    
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request,'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
     
-    
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+        
+    else:
+        request.session['last_visit'] = last_visit_cookie
+        
+    request.session['visits'] = visits
     
     
     
